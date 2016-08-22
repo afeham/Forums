8bcb28ce93bbc7cbcdbc1dee6ea2221cf5f64a5a
evilsauce <evilsauce@users.noreply.github.com>
2016-08-21 22:32:15 -0400

    Update base.php

--- base.php
+++ base.php
@@ -2,9 +2,9 @@
 session_start();
 
 $dbhost = "localhost";
-$dbname = "website";
-$dbuser = "development";
-$dbpass = "development";
+$dbname = "mcvergec_dev";
+$dbuser = "mcvergec_dev";
+$dbpass = "saku1712";
 
 $websql = mysql_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysql_error());
 mysql_select_db($dbname, $websql) or die("MySQL Error: " . mysql_error());
