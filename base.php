229a39fed301b01c5a40e7f3e7ca35264ed305b1
sakun <7917503+sakunc@users.noreply.github.com>
2018-06-11 21:47:58 -0400

    Update base.php

--- base.php
+++ base.php
@@ -2,9 +2,9 @@
 session_start();
 
 $dbhost = "localhost";
-$dbname = "mcvergec_dev";
-$dbuser = "mcvergec_dev";
-$dbpass = "saku1712";
+$dbname = "lolnah";
+$dbuser = "lolnah";
+$dbpass = "lolnah";
 
 $websql = mysql_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysql_error());
 mysql_select_db($dbname, $websql) or die("MySQL Error: " . mysql_error());
